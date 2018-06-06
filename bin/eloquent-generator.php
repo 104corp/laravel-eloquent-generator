ecf80ffcd0090d74b477828df8280db4fc3032a0
MilesChou <jangconan@gmail.com>
2018-06-07 00:43:27 +0800

    Require Doctrine DBAL package

--- bin/eloquent-generator.php
+++ bin/eloquent-generator.php
@@ -2,5 +2,27 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
+// Configure database connections
+$connections = [
+    'default' => [
+        'driver' => 'mysql',
+        'host' => '127.0.0.1',
+        'port' => '3306',
+        'database' => 'default',
+        'username' => 'root',
+        'password' => 'password',
+        'charset' => 'utf8mb4',
+        'collation' => 'utf8mb4_unicode_ci',
+    ],
+];
+
+$capsule = new \Illuminate\Database\Capsule\Manager;
+
+foreach ($connections as $connectionName => $setting) {
+    $capsule->addConnection($setting, $connectionName);
+}
+
+$capsule->setAsGlobal();
+
 $app = new \Corp104\Eloquent\Generator\App();
 $app->run();
